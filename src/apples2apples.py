4135b597ed12bad1142e55fbc47452219eab8ed3
Santeri Paajanen <paajanensanteri@gmail.com>
2022-10-28 14:31:03 +0300

    Resids are now taken from the MDAnalysis group

--- src/apples2apples.py
+++ src/apples2apples.py
@@ -4,15 +4,17 @@ import Bio.SeqIO
 from Bio.Align.Applications import ClustalOmegaCommandline
 from Bio.SeqRecord import SeqRecord
 
+from . import utils
+
 
 def read_seqs_from_aligment_file(aligned_fasta_file: str, ids: List[str]) -> List[str]:
 
     seqs = ['' for _ in ids]
-
+    i_seq = -1
     with open(aligned_fasta_file, 'r') as file:
         for line in file:
             if line[:4] == '>pdb':
-                i_seq = int(line[4])-1
+                i_seq += 1
             else:
                 # [:-1] to remove \n from the end of the line
                 seqs[i_seq] += line[:-1]
@@ -32,49 +34,52 @@ def aligned_sequences(ids: List[str], records: List[SeqRecord], temp: str):
     return read_seqs_from_aligment_file(aligned_fasta_file, ids)
 
 
-def find_apples2apples(seqs: List[str], indeces: List[int], not_aligned_sel: str) -> List[str]:
+def find_apples2apples(seqs: List[str], resids: List[List[int]], not_aligned_sel: str) -> List[str]:
 
-    indeces = np.asarray(indeces)
+    indeces = np.zeros(len(seqs), dtype=int)
 
-    backbones = ['' for _ in seqs]
-    whole_ress = ['' for _ in seqs]
+    backbones = [[] for _ in seqs]
+    whole_ress = [[] for _ in seqs]
 
     for aas in zip(*seqs):
         aas = np.asarray(aas)
 
-        bools = np.array([aa != '-' for aa in aas])
+        bools = aas != '-'
 
         if np.all(bools):
 
-            indeces = indeces + 1
-
             if np.all(aas == aas[0]):
                 for i, index in enumerate(indeces):
-                    whole_ress[i] += ' {}'.format(index)
+                    whole_ress[i].append(resids[i][index])
 
             else:
                 for i, index in enumerate(indeces):
-                    backbones[i] += ' {}'.format(index)
+                    backbones[i].append(resids[i][index])
 
+            indeces = indeces + 1
         else:
-            for i, booL in enumerate(bools):
-                if booL:
-                    indeces[i] += 1
+            indeces += bools
 
+    print(f"Found {len(whole_ress[0])} common residues "
+          f"and {len(backbones[0])} alignable but different")
     sels = []
     for backbone, whole_res in zip(backbones, whole_ress):
-        if backbone == '':
-            if whole_res == '':
+        if not backbone:
+            if not whole_res:
                 sel = ''
             else:
-                sel = 'resid {}'.format(whole_res)
+                sel = 'resid {}'.format(
+                    utils.compressed_resid_list(whole_res))
 
-        elif whole_res == '':
-            sel = '{} and resid {}'.format(not_aligned_sel, backbone)
+        elif not whole_res:
+            sel = '{} and resid {}'.format(
+                not_aligned_sel,
+                utils.compressed_resid_list(backbone))
         else:
             sel = '({} and resid {}) or (resid {})'.format(
-                not_aligned_sel, backbone, whole_res)
-            #sel = 'resid {}'.format(whole_res)
+                not_aligned_sel,
+                utils.compressed_resid_list(backbone),
+                utils.compressed_resid_list(whole_res))
 
         sels.append(sel)
 
